0bae3e38d72c2e5b2765f27a3d473db05eb16c5a
Rohan-AI-Enthusiast <Rohan@placifyrecruitment.com>
2025-12-17 17:32:37 +1100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
-import { Nav } from "@/components/site/Nav";
 import { ScrollFrameScene } from "@/components/site/ScrollFrameScene";
 
 export default function Page() {
-  return (
-    <main>
-      <Nav />
-      <ScrollFrameScene />
-    </main>
-  );
+  return <ScrollFrameScene />;
 }
